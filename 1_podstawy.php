8b5ae49048b80b25d94486f9f6fdfa670ec179a2
Danylo Yakovenko <dyakovenko@edu.cdv.pl>
2023-03-06 13:38:11 +0100

    what

---
+++ 1_podstawy.php
@@ -0,0 +1,29 @@
+<?php
+        $firstName = "Janusz";
+        $lastName = "Nowak";
+        echo "Imię i nazwisko: $firstName $lastName<br>";
+        echo 'Imię i nazwisko: $firstName $lastName<br>';
+//      heredoc
+       echo <<< DATA
+          Imię: $firstName<br>
+          Nazwisko: $lastName<br>
+DATA;
+        $data = <<< DATA
+          Imię: $firstName<br>
+          Nazwisko: $lastName<br>
+DATA; 
+
+//nowdoc
+echo <<< 'DATA'
+        Imię: $firstName<br>
+        Nazwisko: $lastName<br>
+DATA;
+
+
+$bin = 0b1011;
+echo $bin; //11
+$oct = 0o11;
+echo $oct; //9
+$hex = 0xA1;
+echo $hex; //161
+?>
